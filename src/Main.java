2c124706af253c297d4032afd44f3f29c0d56d94
Kanyaton Somjit <sita@MacBook-Pro-som-tillhor-Kanyaton.local>
2022-09-15 18:55:24 +0200

    more chang

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-import java.awt.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
+         Rectangle r1 = new Rectangle();
+         Rectangle r2 = new Rectangle(2,4);
+        Rectangle r3 = new Rectangle(3,4);
 
-        Rectangle r1 = new Rectangle();
-        Rectangle r2 = new Rectangle(1,2,3,4);
-        Rectangle r3= new Rectangle(5,6);
+        System.out.println(r1);
+        System.out.println (r2.calculateArea(r2.width,r2.height));
+        System.out.println(r3.calculatePerimeter(r3.width,r3.height));
 
-
-        System.out.println(r2.calculateArea(5, 6));
 
     }
 }
